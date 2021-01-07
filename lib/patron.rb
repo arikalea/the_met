9b656f6b2090807c3a91bdba57ef2ab4fbc1b8bc
Arika Turing <ablenker@gmail.com>
2021-01-07 09:08:08 -0700

    Initial commit

---
+++ lib/patron.rb
@@ -0,0 +1,4 @@
+class Exhibit
+
+
+end 
---
+++ test/exhibit_test.rb
@@ -0,0 +1,15 @@
+require './lib/exhibit'
+require './lib/patron'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class ExhibitTest < Minitest:: Test
+
+  def test_it_exisits_and_has_attributes
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+
+    assert_instance_of Exhibit, exhibit
+    assert_equal "Gems and Minerals", exhibit.name
+    assert_equal 0, exhibit.cost
+  end
+end 
