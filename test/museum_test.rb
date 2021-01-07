475b362c8f34c7a786549575c0effb8e58097ef2
Arika Turing <ablenker@gmail.com>
2021-01-07 10:01:03 -0700

    Create no patrons test

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -37,8 +37,8 @@ class MuseumTest < Minitest:: Test
     patron_1 = Patron.new("Bob", 20)
     patron_2 = Patron.new("Sally", 20)
 
-    dmns.add_exhibit(gems_and_minerals)
     dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(gems_and_minerals)
     dmns.add_exhibit(imax)
     patron_1.add_interest("Dead Sea Scrolls")
     patron_1.add_interest("Gems and Minerals")
@@ -46,5 +46,18 @@ class MuseumTest < Minitest:: Test
 
     assert_equal [dead_sea_scrolls, gems_and_minerals], dmns.recommend_exhibits(patron_1)
     assert_equal [imax], dmns.recommend_exhibits(patron_2)
+  end
+
+  def test_museum_starts_without_patrons
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+
+    assert_equal [], dmns.patrons 
   end
 end
